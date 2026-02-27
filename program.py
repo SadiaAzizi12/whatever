a33525fc17d7b914e335032a9e324228fe24b580
nam452 <nam452@teach-cpu-n3.crc.pitt.edu>
2026-02-27 10:20:58 -0500

    ??

--- program.py
+++ program.py
@@ -1,1 +1,9 @@
+#This is our program
 firstNum = input("Enter a num please")
+secondNum = input("Enter a second num please")
+
+firstNum = int(firstNum)
+secondNum =int(secondNum)
+
+print("This is adding the two numbers together", firstNum+secondNum)
+print("This is subtracting the second one from the first one", firstNum-secondNum)
